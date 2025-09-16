63402ccde2bc6a6aea1a2ae50c835a4025cef797
Tyler Fox <tyler@blur.com>
2025-09-15 18:10:13 -0700

    Switch to a layout that our build script understands

---
+++ quick_compile.bat
@@ -0,0 +1,25 @@
+setlocal
+
+SET MAYA_VERSION=2024
+REM "vs" "ninja"
+REM use VS for the debugger, otherwise use NINJA
+REM Until I figure out how to debug using nvim
+SET BACKEND=ninja
+REM "debug" "debugoptimized" "release"
+SET BUILDTYPE=release
+SET BUILDDIR=mayabuild_%BUILDTYPE%_%MAYA_VERSION%_%BACKEND%
+
+if not exist %BUILDDIR%\ (
+    meson setup %BUILDDIR% ^
+    -Dmaya:maya_version=%MAYA_VERSION% ^
+    --buildtype %BUILDTYPE% ^
+    --backend %BACKEND% ^
+    --vsenv
+)
+
+if exist %BUILDDIR%\ (
+    meson compile -C %BUILDDIR%
+    meson install --skip-subprojects -C %BUILDDIR%
+)
+
+pause
